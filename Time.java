c398290affcbea50c8c0222eba55d61b82d03f99
Burdentoearth <108297041+Burdentoearth@users.noreply.github.com>
2022-06-27 23:56:05 +0530

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,22 @@
+public class Time {
+    public static void main(String[] args) {
+
+        // my time is now
+        System.out.println("Hello");
+
+        //variables
+        int a=1, b=2, c=3;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("a = "+a +" b = "+b + " c = "+c);
+        
+        //maths variables
+        System.out.println("a + b = " + a+b);
+        System.out.println("b / a = " + b/a);
+        System.out.println("c * a = " + c*a);
+        System.out.println("c % b = " + c%b);
+    
+
+    }
+}
